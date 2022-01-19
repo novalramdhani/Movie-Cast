20c4fe043997770573fdd9034875a79f3e848454
novalramdhani <novalfitrahramdhani@gmail.com>
2022-01-20 00:23:08 +0700

    adding spinner loading search dropdown

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,21 @@ module.exports = {
     "./resources/**/*.vue",
   ],
   theme: {
+    spinner: (theme) => ({
+        default: {
+          color: '#dae1e7', // color you want to make the spinner
+          size: '1em', // size of the spinner (used for both width and height)
+          border: '2px', // border-width of the spinner (shouldn't be bigger than half the spinner's size)
+          speed: '500ms', // the speed at which the spinner should rotate
+        },
+    }),
     extend: {
         fontFamily: {
             sans: ['Inter', ...defaultTheme.fontFamily.sans],
         },
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-spinner')({ className: 'spinner', themeKey: 'spinner' }),
+  ],
 }
